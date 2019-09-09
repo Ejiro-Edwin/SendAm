9497b1ed995c604be91d601dc506ca3acfe9fa1e
Ejiro Edwin Obamrevwo <EjiroEdwin@gmail.com>
2019-09-09 15:15:08 +0100

    Updated online mysql DB

--- Server/config/db.js
+++ Server/config/db.js
@@ -3,7 +3,7 @@ const db = {}
 const sequelize = new Sequelize('kkaUAdJbRK', 'kkaUAdJbRK', 'bPOh86jri0', {
   host: 'remotemysql.com',
   dialect: 'mysql',
-  
+
   operatorsAliases: false,
 
   pool: {
@@ -13,18 +13,6 @@ const sequelize = new Sequelize('kkaUAdJbRK', 'kkaUAdJbRK', 'bPOh86jri0', {
     idle: 10000
   }
 })
-// const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, '', {
-//   host: process.env.DATABASE || 'localhost',
-//   dialect: 'mysql',
-//   operatorsAliases: false,
-
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// })
 
 db.sequelize = sequelize
 db.Sequelize = Sequelize
